f2c83d454bf540ed249ff73f9a29a7c450afacfe
Luan Lopes <luanglopes@gmail.com>
2020-05-30 20:21:05 -0300

    implemen role based authorization

--- src/modules/books/infra/http/routes/books.router.ts
+++ src/modules/books/infra/http/routes/books.router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
+import ensureHasRole from '@modules/users/infra/http/middlewares/ensureHasRole'
+import EUserRoles from '@modules/users/enums/EUserRoles'
 import BookController from '../controllers/BookController'
 
 const booksRouter = Router()
@@ -8,8 +10,23 @@ const bookController = new BookController()
 
 booksRouter.get('/', bookController.index)
 booksRouter.get('/:id', bookController.getOne)
-booksRouter.post('/', ensureAuthenticated, bookController.create)
-booksRouter.put('/:id', ensureAuthenticated, bookController.update)
-booksRouter.delete('/:id', ensureAuthenticated, bookController.delete)
+booksRouter.post(
+  '/',
+  ensureAuthenticated,
+  ensureHasRole(EUserRoles.admin, EUserRoles.librarian),
+  bookController.create,
+)
+booksRouter.put(
+  '/:id',
+  ensureAuthenticated,
+  ensureHasRole(EUserRoles.admin, EUserRoles.librarian),
+  bookController.update,
+)
+booksRouter.delete(
+  '/:id',
+  ensureAuthenticated,
+  ensureHasRole(EUserRoles.admin, EUserRoles.librarian),
+  bookController.delete,
+)
 
 export default booksRouter
--- src/modules/users/infra/http/routes/users.router.ts
+++ src/modules/users/infra/http/routes/users.router.ts
@@ -1,15 +1,32 @@
 import { Router } from 'express'
 
+import EUserRoles from '@modules/users/enums/EUserRoles'
 import UserController from '../controllers/UserController'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import ensureHasRole from '../middlewares/ensureHasRole'
 
 const usersRouter = Router()
 const userController = new UserController()
 
 usersRouter.get('/', userController.index)
 usersRouter.get('/:id', userController.getOne)
-usersRouter.post('/', ensureAuthenticated, userController.create)
-usersRouter.put('/:id', ensureAuthenticated, userController.update)
-usersRouter.delete('/:id', ensureAuthenticated, userController.delete)
+usersRouter.post(
+  '/',
+  ensureAuthenticated,
+  ensureHasRole(EUserRoles.admin),
+  userController.create,
+)
+usersRouter.put(
+  '/:id',
+  ensureAuthenticated,
+  ensureHasRole(EUserRoles.admin),
+  userController.update,
+)
+usersRouter.delete(
+  '/:id',
+  ensureAuthenticated,
+  ensureHasRole(EUserRoles.admin),
+  userController.delete,
+)
 
 export default usersRouter
