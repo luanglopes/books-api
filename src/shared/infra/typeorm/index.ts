4bcf87c9318aad8a72e2b874816577facb3e2046
Luan Lopes <luanglopes@gmail.com>
2020-06-01 15:38:38 -0300

    fix database config paths to build

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,15 +1,16 @@
-import { createConnection, getConnectionOptions } from 'typeorm'
+import { createConnection, getConnectionOptions, EntitySchema } from 'typeorm'
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+const adjustPathsToBuild = (path: string | Function | EntitySchema): string =>
+  path.toString().replace('src', 'dist').replace('.ts', '.js')
 
 const initDatabase = async (): Promise<void> => {
   const connOptions = await getConnectionOptions()
 
   if (process.env.NODE_ENV === 'production') {
     Object.assign(connOptions, {
-      entities: [
-        connOptions.entities?.map((path) =>
-          path.toString().replace('src', 'dist'),
-        ),
-      ],
+      entities: connOptions.entities?.map(adjustPathsToBuild),
+      migrations: connOptions.migrations?.map(adjustPathsToBuild),
     })
   }
 
