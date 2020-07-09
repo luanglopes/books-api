1a63374009d44d1c84c4150e58cd6c8469df7379
Luan Lopes <luanglopes@gmail.com>
2020-07-08 23:03:52 -0300

    create fakes

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -6,7 +6,7 @@ import IUsersRepository from '../IUsersRepository'
 export default class FakeUsersRepository implements IUsersRepository {
   private users: Array<IUserEntity> = []
 
-  async findById(id: number): Promise<IUserEntity | undefined> {
+  async findById(id: IUserEntity['id']): Promise<IUserEntity | undefined> {
     return this.users.find((user) => user.id === id)
   }
 
@@ -21,4 +21,40 @@ export default class FakeUsersRepository implements IUsersRepository {
 
     return newUser
   }
+
+  async addFavoriteBook(): Promise<void> {}
+
+  async delete(id: IUserEntity['id']): Promise<void> {
+    this.users = this.users.filter((user) => user.id !== id)
+  }
+
+  async findByEmail(
+    email: IUserEntity['email'],
+  ): Promise<IUserEntity | undefined> {
+    return this.users.find((user) => user.email === email)
+  }
+
+  async findByPhone(
+    phone: IUserEntity['phone'],
+  ): Promise<IUserEntity | undefined> {
+    return this.users.find((user) => user.phone === phone)
+  }
+
+  async list(): Promise<Array<IUserEntity>> {
+    return this.users
+  }
+
+  async update(data: IUserEntity): Promise<IUserEntity> {
+    const { id } = data
+
+    const userIndex = this.users.findIndex((user) => user.id === id)
+
+    const user = this.users[userIndex]
+
+    this.users[userIndex] = { ...user, ...data }
+
+    return this.users[userIndex]
+  }
+
+  async removeFavoriteBook(): Promise<void> {}
 }
