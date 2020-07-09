60589880092ecb44708036d31da21376d4a79775
Luan Lopes <luanglopes@gmail.com>
2020-07-08 23:04:25 -0300

    create VerifyAuthTokenService unit test

---
+++ src/modules/users/services/VerifyAuthTokenService.spec.ts
@@ -0,0 +1,110 @@
+import AppError from '@shared/errors/AppError'
+import VerifyAuthTokenService from './VerifyAuthTokenService'
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
+import FakeTokenProvider from '../providers/TokenProvider/fakes/FakeTokenProvider'
+import EUserRoles from '../enums/EUserRoles'
+
+describe('VeridyAuthTokenService', () => {
+  it('should return a user if valid authotizationHeader is provided', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeTokenProvider = new FakeTokenProvider()
+
+    const verifyAuthTokenService = new VerifyAuthTokenService(
+      fakeUsersRepository,
+      fakeTokenProvider,
+    )
+
+    const user = await fakeUsersRepository.create({
+      birthday: new Date(),
+      email: 'jon@email.com',
+      name: 'Jon Doe',
+      password: 'fake-password',
+      phone: '99999999',
+    })
+    const token = await fakeTokenProvider.generateToken(user)
+
+    const result = await verifyAuthTokenService.execute({
+      authorizationHeader: `Bearer ${token}`,
+    })
+
+    expect(result).toEqual(user)
+  })
+
+  it('should throw an error if empty authorizationHeader is provided', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeTokenProvider = new FakeTokenProvider()
+
+    const verifyAuthTokenService = new VerifyAuthTokenService(
+      fakeUsersRepository,
+      fakeTokenProvider,
+    )
+
+    await expect(
+      verifyAuthTokenService.execute({
+        authorizationHeader: '',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should throw an error if no token is provided on authorizationHeader', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeTokenProvider = new FakeTokenProvider()
+
+    const verifyAuthTokenService = new VerifyAuthTokenService(
+      fakeUsersRepository,
+      fakeTokenProvider,
+    )
+
+    await expect(
+      verifyAuthTokenService.execute({
+        authorizationHeader: 'Bearer ',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should throw an error if invalid token is provided on authorizationHeader', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeTokenProvider = new FakeTokenProvider()
+
+    const verifyTokenSpy = jest.spyOn(fakeTokenProvider, 'verifyToken')
+
+    verifyTokenSpy.mockReturnValue(Promise.resolve(undefined))
+
+    const verifyAuthTokenService = new VerifyAuthTokenService(
+      fakeUsersRepository,
+      fakeTokenProvider,
+    )
+
+    await expect(
+      verifyAuthTokenService.execute({
+        authorizationHeader: 'Bearer invalid',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should throw an error if a token for an invalid user provided on authorizationHeader', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeTokenProvider = new FakeTokenProvider()
+
+    const verifyAuthTokenService = new VerifyAuthTokenService(
+      fakeUsersRepository,
+      fakeTokenProvider,
+    )
+
+    const token = await fakeTokenProvider.generateToken({
+      id: 1,
+      password: '',
+      name: '',
+      birthday: new Date(),
+      email: '',
+      phone: '',
+      role: EUserRoles.user,
+    })
+
+    await expect(
+      verifyAuthTokenService.execute({
+        authorizationHeader: `Bearer ${token}`,
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
+})
