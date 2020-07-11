c874743eca3d8a133b64edc1353d4ea04ef98a61
Luan Lopes <luanglopes@gmail.com>
2020-07-11 02:19:54 -0300

    fix type on test variable name

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -10,14 +10,14 @@ describe('AuthenticateUserService', () => {
   let fakeHashProvider: FakeHashProvider
   let fakeTokenProvider: FakeTokenProvider
   let authenticateUserService: AuthenticateUserService
-  let testeUser: IUserEntity
+  let testUser: IUserEntity
 
   beforeEach(async () => {
     fakeUsersRepository = new FakeUsersRepository()
     fakeHashProvider = new FakeHashProvider()
     fakeTokenProvider = new FakeTokenProvider()
 
-    testeUser = await fakeUsersRepository.create({
+    testUser = await fakeUsersRepository.create({
       birthday: new Date(),
       email: 'jon@emial.com',
       name: 'Jon Doe',
@@ -34,19 +34,19 @@ describe('AuthenticateUserService', () => {
 
   it('should authenticate user if valid email and password is provided', async () => {
     const result = await authenticateUserService.execute({
-      email: testeUser.email,
-      password: testeUser.password,
+      email: testUser.email,
+      password: testUser.password,
     })
 
     expect(result).toHaveProperty('token')
-    expect(result).toHaveProperty('user', testeUser)
+    expect(result).toHaveProperty('user', testUser)
   })
 
   it('should not authenticate user if invalid email is provided', async () => {
     await expect(
       authenticateUserService.execute({
         email: 'invalid@email.com',
-        password: testeUser.password,
+        password: testUser.password,
       }),
     ).rejects.toBeInstanceOf(AppError)
   })
@@ -54,7 +54,7 @@ describe('AuthenticateUserService', () => {
   it('should not authenticate user if invalid password is provided', async () => {
     await expect(
       authenticateUserService.execute({
-        email: testeUser.email,
+        email: testUser.email,
         password: 'wrong-password',
       }),
     ).rejects.toBeInstanceOf(AppError)
