f1dc345caf2e67018c4631ad54026cd6992883e3
Luan Lopes <luanglopes@gmail.com>
2020-07-12 05:49:39 -0300

    remove unnecessary lint rule comment

--- src/modules/books/dtos/ICreateBookDTO.ts
+++ src/modules/books/dtos/ICreateBookDTO.ts
@@ -3,6 +3,5 @@ import IBookEntity from '../entities/IBookEntity'
 export default interface ICreateBookDTO {
   title: IBookEntity['title']
   year: IBookEntity['year']
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   ISBN: IBookEntity['ISBN']
 }
