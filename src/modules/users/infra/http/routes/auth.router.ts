106e9b9e2c011370dbbb840f30b2cf6661e42d5a
Luan Lopes <luanglopes@gmail.com>
2020-07-12 04:25:52 -0300

    create auth routes validators

--- src/modules/users/infra/http/routes/auth.router.ts
+++ src/modules/users/infra/http/routes/auth.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
+import { celebrate } from 'celebrate'
 
 import AuthController from '../controllers/AuthController'
+import authValidators from '../validators/auth'
 
 const authRouter = Router()
 const authController = new AuthController()
 
-authRouter.post('/', authController.login)
+authRouter.post('/', celebrate(authValidators.login), authController.login)
 
 export default authRouter
