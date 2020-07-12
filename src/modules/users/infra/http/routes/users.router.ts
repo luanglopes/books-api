e06bbbb3f7645cd3c74457e3ba4526d86911fe8d
Luan Lopes <luanglopes@gmail.com>
2020-07-12 04:17:39 -0300

    craete users routes validators

--- src/modules/users/infra/http/routes/users.router.ts
+++ src/modules/users/infra/http/routes/users.router.ts
@@ -1,24 +1,36 @@
 import { Router } from 'express'
+import { celebrate } from 'celebrate'
 
 import EUserRoles from '@modules/users/enums/EUserRoles'
 import UserController from '../controllers/UserController'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 import ensureHasRole from '../middlewares/ensureHasRole'
+import usersValidators from '../validators/users'
 
 const usersRouter = Router()
 const userController = new UserController()
 
-usersRouter.get('/', userController.index)
-usersRouter.get('/:id', userController.getOne)
-usersRouter.post('/', userController.create)
+usersRouter.get('/', celebrate(usersValidators.index), userController.index)
+
+usersRouter.get(
+  '/:id',
+  celebrate(usersValidators.getOne),
+  userController.getOne,
+)
+
+usersRouter.post('/', celebrate(usersValidators.create), userController.create)
+
 usersRouter.put(
   '/:id',
+  celebrate(usersValidators.update),
   ensureAuthenticated,
   ensureHasRole(EUserRoles.admin),
   userController.update,
 )
+
 usersRouter.delete(
   '/:id',
+  celebrate(usersValidators.delete),
   ensureAuthenticated,
   ensureHasRole(EUserRoles.admin),
   userController.delete,
