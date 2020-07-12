d5c91746bd612287bb2ae83efa070d396486d273
Luan Lopes <luanglopes@gmail.com>
2020-07-12 04:22:35 -0300

    create favorites routes validators

--- src/modules/users/infra/http/routes/favorites.router.ts
+++ src/modules/users/infra/http/routes/favorites.router.ts
@@ -1,15 +1,25 @@
 import { Router } from 'express'
+import { celebrate } from 'celebrate'
 
 import FavoriteController from '../controllers/FavoriteController'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import favoritesValidators from '../validators/favorites'
 
 const favoriteRouter = Router({ mergeParams: true })
 const favoriteController = new FavoriteController()
 
 favoriteRouter.get('/', ensureAuthenticated, favoriteController.index)
-favoriteRouter.post('/', ensureAuthenticated, favoriteController.create)
+
+favoriteRouter.post(
+  '/',
+  celebrate(favoritesValidators.create),
+  ensureAuthenticated,
+  favoriteController.create,
+)
+
 favoriteRouter.delete(
   '/:bookId',
+  celebrate(favoritesValidators.delete),
   ensureAuthenticated,
   favoriteController.delete,
 )
